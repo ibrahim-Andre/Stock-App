36970c3bc14993f996826bc42852e549d098129e
Andre-se <ibrahimtanriover64@gmail.com>
2023-03-22 21:39:56 +0100

    v3

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,23 +1,38 @@
-import Avatar from "@mui/material/Avatar";
-import Box from "@mui/material/Box";
-import Container from "@mui/material/Container";
-import Grid from "@mui/material/Grid";
-import Typography from "@mui/material/Typography";
-import LockIcon from "@mui/icons-material/Lock";
-import image from "../assets/result.svg";
-import { Link, useNavigate } from "react-router-dom";
-import { useSelector } from "react-redux";
-import { Formik, Form } from "formik";
-import TextField from "@mui/material/TextField";
-import { object, string, number, date, InferType } from "yup";
+import Avatar from "@mui/material/Avatar"
+import Box from "@mui/material/Box"
+import Container from "@mui/material/Container"
+import Grid from "@mui/material/Grid"
+import Typography from "@mui/material/Typography"
+import LockIcon from "@mui/icons-material/Lock"
+import image from "../assets/result.svg"
+import { Link, useNavigate } from "react-router-dom"
+import { useSelector } from "react-redux"
+import { Formik, Form } from "formik"
+import TextField from "@mui/material/TextField"
+import { object, string } from "yup"
+import LoadingButton from "@mui/lab/LoadingButton"
+import useAuthCall from "../hooks/useAuthCall"
 
 const Login = () => {
-  const navigate = useNavigate();
-  const { currentUser, error } = useSelector((state) => state?.auth);
-  const loginSchema = object({
-    email: string().email().required(),
-    // password: string().password(),
-  });
+  const navigate = useNavigate()
+  const { currentUser, error, loading } = useSelector((state) => state?.auth)
+
+  const { login } = useAuthCall()
+
+  const loginScheme = object({
+    email: string()
+      .email("Lutfen valid bir email giriniz")
+      .required("Email zorunludur"),
+    password: string()
+      .required("password zorunludur")
+      .min(8, "password en az 8 karakter olmalıdır")
+      .max(20, "password en fazla 20 karakter olmalıdır")
+      .matches(/\d+/, "Password bir sayı içermelidir")
+      .matches(/[a-z]/, "Password bir küçük harf içermelidir")
+      .matches(/[A-Z]/, "Password bir büyük harf içermelidir")
+      .matches(/[!,?{}><%&$#£+-.]+/, "Password bir özel karakter içermelidir"),
+  })
+
   return (
     <Container maxWidth="lg">
       <Grid
@@ -27,7 +42,8 @@ const Login = () => {
         sx={{
           height: "100vh",
           p: 2,
-        }}>
+        }}
+      >
         <Grid item xs={12} mb={3}>
           <Typography variant="h3" color="primary" align="center">
             STOCK APP
@@ -41,26 +57,31 @@ const Login = () => {
               m: "auto",
               width: 40,
               height: 40,
-            }}>
+            }}
+          >
             <LockIcon size="30" />
           </Avatar>
           <Typography
             variant="h4"
             align="center"
             mb={4}
-            color="secondary.light">
+            color="secondary.light"
+          >
             Login
           </Typography>
+
           <Formik
             initialValues={{ email: "", password: "" }}
-            validationSchema={loginSchema}
+            validationSchema={loginScheme}
             onSubmit={(values, actions) => {
-              actions.resetForm();
-              actions.setSubmitting();
-            }}>
+              login(values)
+              actions.resetForm()
+              actions.setSubmitting(false)
+            }}
+          >
             {({ values, handleChange, handleBlur, errors, touched }) => (
               <Form>
-                <Box sx={{ display: "flex", flexDirection: "column" }}>
+                <Box sx={{ display: "flex", flexDirection: "column", gap: 2 }}>
                   <TextField
                     label="Email"
                     name="email"
@@ -73,6 +94,26 @@ const Login = () => {
                     error={touched.email && Boolean(errors.email)}
                     helperText={touched.email && errors.email}
                   />
+                  <TextField
+                    label="Password"
+                    name="password"
+                    id="password"
+                    type="password"
+                    variant="outlined"
+                    value={values?.password || ""}
+                    onChange={handleChange}
+                    onBlur={handleBlur}
+                    error={touched.password && Boolean(errors.password)}
+                    helperText={touched.password && errors.password}
+                  />
+
+                  <LoadingButton
+                    type="submit"
+                    variant="contained"
+                    loading={loading}
+                  >
+                    Submit
+                  </LoadingButton>
                 </Box>
               </Form>
             )}
@@ -90,7 +131,7 @@ const Login = () => {
         </Grid>
       </Grid>
     </Container>
-  );
-};
+  )
+}
 
-export default Login;
+export default Login
